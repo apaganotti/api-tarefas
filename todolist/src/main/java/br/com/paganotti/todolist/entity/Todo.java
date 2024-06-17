cbc31934b3cf65e51d86f67832e72138a709c3d9
apaganotti <andredesouzapaganotti@gmail.com>
2024-06-17 10:41:46 -0300

    initialized entity

---
+++ todolist/src/main/java/br/com/paganotti/todolist/entity/Todo.java
@@ -0,0 +1,19 @@
+package br.com.paganotti.todolist.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "todos")
+public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String descricao;
+    private boolean realizado;
+    private int prioridade;
+}
